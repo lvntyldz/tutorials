b8e2f7ecdf3b9224e9ea07745ae1448cb38e6b26
levent yıldız <lewenty@gmail.com>
2020-04-30 11:12:29 +0300

    initial commit

---
+++ react-native-hooks-examples/src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+export default class Wrapper extends Component {
+
+    render() {
+        return (
+            <SafeAreaView>
+                <Text>Hello!</Text>
+            </SafeAreaView>
+        )
+    }
+
+}
