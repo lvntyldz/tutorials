59eb7e488af696e5b13cff584be64e049ee4c798
levent yıldız <lewenty@gmail.com>
2020-06-12 13:48:30 +0300

    increase and decreease button event listener

--- kotlin-android-app-examples/01-hello-world/app/src/main/java/com/tutorials/helloworld/MainActivity.kt
+++ kotlin-android-app-examples/01-hello-world/app/src/main/java/com/tutorials/helloworld/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.tutorials.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var number = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.counterTxt) as TextView
+        var increaseBtn: Button = findViewById(R.id.increaseBtn) as Button
+        var decreaseBtn: Button = findViewById(R.id.decreaseBtn) as Button
+
+        increaseBtn.setOnClickListener {
+            number = number + 1;
+            textView.text = number.toString();
+        }
+
+        decreaseBtn.setOnClickListener {
+            number = number - 1;
+            textView.text = number.toString();
+        }
+
     }
 }
+
