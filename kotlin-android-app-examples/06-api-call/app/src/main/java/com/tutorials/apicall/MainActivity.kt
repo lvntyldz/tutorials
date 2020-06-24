690b5a25b9934542bbd850dee2a21123b14d8ef9
levent yıldız <lewenty@gmail.com>
2020-06-24 10:04:39 +0300

    kotlin- fetch data from api

--- kotlin-android-app-examples/06-api-call/app/src/main/java/com/tutorials/apicall/MainActivity.kt
+++ kotlin-android-app-examples/06-api-call/app/src/main/java/com/tutorials/apicall/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.tutorials.apicall
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import okio.IOException
 
 class MainActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        run("https://jsonplaceholder.typicode.com/photos")
+    }
+
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Error! e : ${e.message} ")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                println("Success...")
+                println(response.body?.string())
+            }
+        })
     }
 }
