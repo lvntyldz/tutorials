559f504028daaff00fd4795f2371673167b88155
levent yıldız <lewenty@gmail.com>
2020-06-30 11:17:49 +0300

    kotlin dateTimePicker add TimePicker

--- kotlin-android-app-examples/08-date-time-picker/app/src/main/java/com/tutorials/datetimepicker/MainActivity.kt
+++ kotlin-android-app-examples/08-date-time-picker/app/src/main/java/com/tutorials/datetimepicker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.tutorials.datetimepicker
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,9 +14,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         dateTextView!!.text = "--/--/----"
+        timeTextView!!.text = "--:--"
+
+        val now = Calendar.getInstance()
 
         changeDateBtn.setOnClickListener {
-            val now = Calendar.getInstance()
             val datePicker = DatePickerDialog(this,
                     DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
                         dateTextView!!.text = "$dayOfMonth/$month/$year"
@@ -23,6 +26,18 @@ class MainActivity : AppCompatActivity() {
                     now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH)
             )
             datePicker.show()
+        }
+
+        changeTimeBtn.setOnClickListener {
+
+            val timePicker = TimePickerDialog(this,
+                    TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                        timeTextView!!.text = "$hourOfDay:$minute"
+                    },
+                    now.get(Calendar.HOUR_OF_DAY),
+                    now.get(Calendar.MINUTE), false)
+            timePicker.show()
+
         }
 
     }
