26e9bb3ebeb84106d49aa9b205148b9da810b1d0
levent yıldız <lewenty@gmail.com>
2020-06-30 12:56:54 +0300

    kotlin progressBar add progressbar to screen

--- kotlin-android-app-examples/09-progressbar/app/src/main/java/com/tutorials/progressbar/MainActivity.kt
+++ kotlin-android-app-examples/09-progressbar/app/src/main/java/com/tutorials/progressbar/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.tutorials.progressbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var i = 0
+    private val handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startProgressBtn.setOnClickListener {
+
+            progressBarComp.setVisibility(View.VISIBLE)
+
+            i = progressBarComp!!.progress
+
+            Thread(Runnable {
+
+                while (i < 100) {
+                    i += 5
+                    handler.post(Runnable {
+                        progressBarComp!!.progress = i
+                        progressBarStatusText!!.text = i.toString() + "/" + progressBarComp!!.max
+                    })
+                    try {
+                        Thread.sleep(100)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
+                }
+
+                if (i == 100) {
+                    progressBarComp.setVisibility(View.INVISIBLE)
+                }
+
+            }).start()
+
+
+        }
     }
+
 }
