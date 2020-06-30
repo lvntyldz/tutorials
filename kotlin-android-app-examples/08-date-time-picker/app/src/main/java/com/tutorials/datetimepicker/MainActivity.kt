27690101d042c24a8109c04c0bd14855a47b1638
levent yıldız <lewenty@gmail.com>
2020-06-30 11:06:24 +0300

    kotlin dateTimePicker add dateDialog

--- kotlin-android-app-examples/08-date-time-picker/app/src/main/java/com/tutorials/datetimepicker/MainActivity.kt
+++ kotlin-android-app-examples/08-date-time-picker/app/src/main/java/com/tutorials/datetimepicker/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.tutorials.datetimepicker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dateTextView!!.text = "--/--/----"
+
+        changeDateBtn.setOnClickListener {
+            val now = Calendar.getInstance()
+            val datePicker = DatePickerDialog(this,
+                    DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                        dateTextView!!.text = "$dayOfMonth/$month/$year"
+                    },
+                    now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH)
+            )
+            datePicker.show()
+        }
+
     }
 }
