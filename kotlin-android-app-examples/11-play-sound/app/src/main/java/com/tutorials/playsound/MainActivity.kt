55a65b0c3076083d55c18f60b471d369dd8d92ea
levent yıldız <lewenty@gmail.com>
2020-07-03 16:57:59 +0300

    kotlin play sound  added

--- kotlin-android-app-examples/11-play-sound/app/src/main/java/com/tutorials/playsound/MainActivity.kt
+++ kotlin-android-app-examples/11-play-sound/app/src/main/java/com/tutorials/playsound/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.tutorials.playsound
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playSoundBtn.setOnClickListener {
+
+            mediaPlayer = MediaPlayer.create(this, R.raw.alert)
+            mediaPlayer?.start()
+        }
     }
 }
